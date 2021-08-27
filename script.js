4e286033bdf5496ced8ac840f1620a7dbe51e065
Anusha <anushamr141022@gmail.com>
2021-08-27 12:17:55 +0530

    add maps

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function gMap(){
 
     var mapProp = {
-        center:new google.maps.LatLng(27.0,0.0),zoom:5,
+        center:new google.maps.LatLng(51.508742,-0.120850),zoom:5,
     };
 
     var map = new google.maps.map(document.getElementById("gmap"),mapProp);
